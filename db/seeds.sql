bffb81646aff45b3dcfa302d86fcae86ff2c2fbe
shayshae5482 <shayshae5482@gmail.com>
2018-09-19 18:40:54 -0500

    server and connection working

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Baconator');
+INSERT INTO burgers (burger_name) VALUES ('Triple cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('California burger');
+INSERT INTO burgers (burger_name) VALUES ('Vegan burger');
